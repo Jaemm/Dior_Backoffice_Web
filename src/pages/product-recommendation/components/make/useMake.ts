291880a4801826b6ec92bb380494a163f95e768b
bekzodchowis <bekzod@chowis.com>
2023-04-05 15:53:17 +0500

    feat: alphabetically product recommendation

--- src/pages/product-recommendation/components/make/useMake.ts
+++ src/pages/product-recommendation/components/make/useMake.ts
@@ -1,8 +1,15 @@
-import { useMutation, useQuery } from '@tanstack/react-query'
 import {
 	getProductFormRecommendation,
 	getProductFormRecommendations,
 } from 'api/product-recommendations'
+import { useMutation, useQuery } from '@tanstack/react-query'
+
+interface IProduct {
+	countries: []
+	id: number
+	name: string
+	routine: string
+}
 
 export const useMake = () => {
 	const { data = { data: { data: [] } }, isLoading } = useQuery(
@@ -11,6 +18,19 @@ export const useMake = () => {
 			getProductFormRecommendations({
 				list_type: 'makeup',
 			}),
+		{
+			select: res => {
+				const newData = res.data.data.sort(function (product1: IProduct, product2: IProduct) {
+					const productName1 = product1.name.toLowerCase()
+					const productName2 = product2.name.toLowerCase()
+					if (productName1 < productName2) return -1
+					if (productName1 > productName2) return 1
+					return 0
+				})
+
+				return { ...res, data: { ...res.data, data: newData } }
+			},
+		},
 	)
 
 	const { mutate, isLoading: isLoadingMutate } = useMutation(getProductFormRecommendation)
